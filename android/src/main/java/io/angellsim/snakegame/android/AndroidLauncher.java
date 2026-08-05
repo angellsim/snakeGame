c085ea93c13f01807496880e989de8369fae3a07
raiden <alicewolffernandes@gmail.com>
2026-08-05 19:00:11 -0300

    chore: initial LibGDX setup

---
+++ android/src/main/java/io/angellsim/snakegame/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package io.angellsim.snakegame.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import io.angellsim.snakegame.SnakeGame;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new SnakeGame(), configuration);
+    }
+}
